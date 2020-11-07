5319a10cee23e98154b754aceff3748273221f6a
April <alagnevall@gmail.com>
2020-11-07 14:17:11 -0600

    adding resources and modules to code

---
+++ PyBank/main.py
@@ -0,0 +1,28 @@
+#put modules in
+import os
+import csv
+
+#create paths to resources 
+csvpath = os.path.join('..','Resources','budget_data.csv')
+
+#variable for total number of months
+Months = 
+#Net total of Profit/Loss over the whole period
+
+#Changes in Profit/Loss  over the entire period
+
+    #average of those changes
+
+#Find the greatest increase in profit over the whole period
+    #date
+    #amount
+
+#Find the greatest decrease in losses over the whole period
+    #date
+    #amount
+
+#Print the analysis to the terminal
+
+#Export a text file with the results
+
+
---
+++ PyPoll/main.py
@@ -0,0 +1,43 @@
+#put modules in
+import os
+import csv
+
+#create paths to resources 
+csvpath = os.path.join('..','Resources','election_data.csv')
+
+#variable for total count
+
+#create and empty list for candidate options
+
+#create a dictionary for candidate votes
+
+#creae a string variable for winning candidate
+
+#create a winning count integer variable
+
+#read the csv and covert it into a list of dictionaries
+
+#read the header
+
+#for loop each row
+
+#add to the total number of votes cast
+
+#list all cadidates who received votes
+
+#if candidate doesnt exist in list then add the candidate
+    #add candidate
+    #put candidate votes into dictionary to start tracking
+#add vote to candidate count
+#print results and send to text files
+
+#print final vote count to terminal
+
+#print final vote count to text files
+
+#loop to find the winner
+
+#print the winning candidate
+
+
+
